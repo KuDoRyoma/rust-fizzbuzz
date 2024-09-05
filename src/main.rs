57a1bbcad29b1f71a08eb9d708c6ec07dff925ee
kudo <31415.Ryoma@gmail.com>
2024-09-05 10:28:22 +0000

    update
    fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    fizzbuzz();
+}
+
+fn fizzbuzz() {
+    for x in 1..101 {
+        // 1から100まで繰り返す
+        if x % 15 == 0 {
+            println!("FizzBuzz");
+        } else if x % 3 == 0 {
+            println!("Fizz");
+        } else if x % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", x);
+        }
+    }
 }
